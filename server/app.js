94ee89c88da8a548168c22df222f3bdc2cb17aae
J-R4 <JoshuaFanera@gmail.com>
2021-03-04 01:47:43 +0700

    perfecting in bulma and adding jquery,ajax,google sign in, trying oauth sign in

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000   
 
 const router = require('./routes/index.js')
 
 const errHandler = require('./middlewares/errHandler.js')
+
+app.use(cors())
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
