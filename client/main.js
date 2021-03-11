b6c269912d21e3556adb76c95361b8ffc20fa879
J-R4 <JoshuaFanera@gmail.com>
2021-03-11 17:05:57 +0700

    deployment

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://localhost:3000'; // ganti dengan url yang heroku
+const baseURL = `https://fensitudu-server.herokuapp.com`; //'http://localhost:3000'; // ganti dengan url yang heroku
 
 $('document').ready(function () {
     view();
